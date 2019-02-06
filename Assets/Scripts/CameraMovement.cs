c9f6e19b16f47cf50f034889c71254fef03dc242
Nathanovich <n.flier@live.nl>
2019-02-06 14:08:01 +0100

    Simple third person controls

--- Assets/Scripts/CameraMovement.cs
+++ Assets/Scripts/CameraMovement.cs
@@ -1,18 +1,33 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 
-public class Camera : MonoBehaviour
-{
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+public class CameraMovement : MonoBehaviour {
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+	[Header("Settings")]
+	[SerializeField] private Transform playerTarget;
+	[SerializeField] private float rotateSpeed;
+
+	[Header("References")]
+	[SerializeField] private Transform playerForward;
+
+	private Vector3 previousPlayerPosition;
+	private Vector3 playerOffset;
+
+	private void Awake() {
+		playerOffset = playerTarget.position - transform.position;
+		previousPlayerPosition = playerTarget.position;
+	}
+
+	private void LateUpdate() {
+		Vector3 playerMovement = playerTarget.position - previousPlayerPosition;
+		transform.position = transform.position + playerMovement;
+		previousPlayerPosition = playerTarget.position;
+
+		Vector3 targetPosition = playerTarget.position + Vector3.up * playerOffset.y;
+		float cameraInput = Input.GetAxis("HorizontalCameraMovement");
+		transform.RotateAround(targetPosition, Vector3.up, rotateSpeed * -cameraInput * Time.deltaTime);
+		transform.LookAt(playerTarget.position);
+
+		playerForward.eulerAngles = new Vector3(0f, transform.eulerAngles.y, 0f);
+	}
+
 }
--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -4,6 +4,7 @@ public class PlayerMovement : MonoBehaviour {
 
 	[Header("Settings")]
 	[SerializeField] private float moveSpeed;
+	[SerializeField] private Transform cameraForward;
 
 	private new Rigidbody rigidbody;
 
@@ -12,15 +13,18 @@ public class PlayerMovement : MonoBehaviour {
 	}
 
 	private void Update() {
-		float horizontalInput = Input.GetAxis("Horizontal");
-		float verticalInput = Input.GetAxis("Vertical");
+		float horizontalInput = Input.GetAxis("HorizontalPlayerMovement");
+		float verticalInput = Input.GetAxis("VerticalPlayerMovement");
 		Vector3 input = new Vector3(horizontalInput, 0f, verticalInput);
-		if (horizontalInput != 0 && verticalInput != 0) {
+		if (horizontalInput != 0f && verticalInput != 0f) {
 			input = Vector3.ClampMagnitude(input, 1f);
 		}
 
-		Vector3 moveForce = input * moveSpeed;
-		rigidbody.AddForce(moveForce);
+		if (input != Vector3.zero) {
+			Vector3 relativeInput = cameraForward.TransformDirection(input);
+			transform.forward = relativeInput;
+			rigidbody.velocity = relativeInput * moveSpeed;
+		}
 	}
 
 }
