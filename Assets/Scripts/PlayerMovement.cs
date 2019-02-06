c8a490f6f7f4e6570e52aa5e3f64754bbfb3c66c
Nathanovich <n.flier@live.nl>
2019-02-06 12:59:51 +0100

    Cameramovement en player movement

---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour {
+
+	[Header("Settings")]
+	[SerializeField] private float moveSpeed;
+
+	private new Rigidbody rigidbody;
+
+	private void Awake() {
+		rigidbody = GetComponent<Rigidbody>();
+	}
+
+	private void Update() {
+		float horizontalInput = Input.GetAxis("Horizontal");
+		float verticalInput = Input.GetAxis("Vertical");
+		Vector3 input = new Vector3(horizontalInput, 0f, verticalInput);
+		if (horizontalInput != 0 && verticalInput != 0) {
+			input = Vector3.ClampMagnitude(input, 1f);
+		}
+
+		Vector3 moveForce = input * moveSpeed;
+		rigidbody.AddForce(moveForce);
+	}
+
+}
